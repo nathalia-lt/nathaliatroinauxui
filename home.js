a6bbb9f26a83066d57a0576955c0243f4195b59a
nathalia <nathaliatroina@gmail.com>
2024-02-09 16:59:42 -0500

    about page

--- home.js
+++ home.js
@@ -1,1 +1,14 @@
-import './style.css'
+
+console.log('home.js');
+const ux_title = document.getElementById('ux_title');
+
+setTimeout(() => {
+    if (ux_title.classList.contains('new-color')) {
+        console.log('should remove new-color');
+        ux_title.classList.remove('new-color');
+    } else {
+        console.log('should add new-color');
+        ux_title.classList.add('new-color');
+    }
+}, 1500);
+
