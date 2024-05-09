3faaa114012f170bb3e8c3c3b246125afc32b8c1
nathalia <nathaliatroina@gmail.com>
2024-05-09 14:26:57 -0400

    small changes

--- projects/yogarythm/yogarythm.js
+++ projects/yogarythm/yogarythm.js
@@ -1,7 +1,11 @@
+import testimonialsData from '../yogarythm/data.js'
+
 
 const menubtn = document.getElementById('hamburguer')
 const navMenu = document.getElementById('nav-menu')
 const menulist = document.getElementById('menu-list')
+
+const carouselItem = document.getElementById('carousel-item')
 
 let hamburguerIsOpen = false;
 
@@ -30,3 +34,34 @@ window.addEventListener('click', (e) => {
         navMenu.classList.remove('active')
     }
 })
+
+// ---------------------------------------
+
+function renderTestimonial(testimonialsData){
+    const userNameEl = document.getElementById('user-name');
+    const carouselItem = document.getElementById('carousel-item')
+    testimonialsData.forEach(testimonial => {
+        console.log(testimonial)
+        userNameEl.textContent = `${testimonial.userName}`
+        carouselItem.appendChild(userNameEl)
+    })
+}
+
+// function renderUserName(testimonialsData){
+//     const userNameEl = document.getElementById('user-name');
+//     userNameEl.textContent = `${testimonialsData.userName}`;
+//     carouselItem.appendChild(userNameEl)
+// }
+
+// function renderTestimonial(testimonialsData){
+//     const carouselItem = document.getElementById('carouselItem')
+//     testimonialsData.forEach(testimonial =>{
+//         carouselItem.innerHTML += testimonial
+//     })
+// }
+
+// renderUserName()
+renderTestimonial()
+console.log(renderTestimonial(testimonialsData))
+
+export default renderUserName
