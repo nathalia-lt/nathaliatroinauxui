36b39a95fa0054cc79a98b4831ccca4eed54b718
nathalia <nathaliatroina@gmail.com>
2024-05-09 19:51:51 -0400

    yogacss

--- projects/yogarythm/data.js
+++ projects/yogarythm/data.js
@@ -1,8 +1,34 @@
 
 
-const testimonialsData = [{userName:'James', review:'I love how this app has different types of yoga practices, from gentle stretching to more intense flows. It keeps my routine fresh and allows me to explore new techniques.'},
-{userName:'Justin', review:'This app offers a variety of yoga routines suitable for all levels. The clear instructions and soothing background music make it easy to follow along, even for beginners.'},
-{userName:'Taylor', review:'As someone who has never done yoga before, this app is a lifesaver! The beginner-friendly tutorials and explanations helped me grasp the basics quickly. I feel more confident and relaxed after each session'},
-{userName:'Suzan', review:' The instructors on this app do an excellent job of breaking down each pose step by step. I never feel lost or confused, which is a common concern for beginners like me. It is like having a patient teacher right in my pocket'}]
+const testimonialsData = [
+    {
+        id: 0,
+        userName: 'James',
+        review: 'I love how this app has different types of yoga practices, from gentle stretching to more intense flows. It keeps my routine fresh and allows me to explore new techniques.',
+        avatar: 'https://randomuser.me/api/portraits/men/75.jpg',
+        date: '31.10.2024'
+    },
+    {
+        id: 1,
+        userName: 'Justin',
+        review: 'This app offers a variety of yoga routines suitable for all levels. The clear instructions and soothing background music make it easy to follow along, even for beginners.',
+        avatar: 'https://randomuser.me/api/portraits/men/63.jpg',
+        date: '08.07.2022'
+    },
+    {
+        id: 2,
+        userName: 'Taylor',
+        review: 'As someone who has never done yoga before, this app is a lifesaver! The beginner-friendly tutorials and explanations helped me grasp the basics quickly. I feel more confident and relaxed after each session',
+        avatar: 'https://randomuser.me/api/portraits/women/20.jpg',
+        date: '30.03.2022'
+    },
+    {
+        id: 3,
+        userName: 'Suzan',
+        review: ' The instructors on this app do an excellent job of breaking down each pose step by step. I never feel lost or confused, which is a common concern for beginners like me. It is like having a patient teacher right in my pocket',
+        avatar: 'https://randomuser.me/api/portraits/women/75.jpg',
+        date: '30.04.2023'
+    }
+]
 
 export default testimonialsData
--- projects/yogarythm/yogarythm.js
+++ projects/yogarythm/yogarythm.js
@@ -1,5 +1,6 @@
 import testimonialsData from '../yogarythm/data.js'
 
+let currentTestimonial = 0;
 
 const menubtn = document.getElementById('hamburguer')
 const navMenu = document.getElementById('nav-menu')
@@ -36,32 +37,94 @@ window.addEventListener('click', (e) => {
 })
 
 // ---------------------------------------
+// 
 
-function renderTestimonial(testimonialsData){
-    const userNameEl = document.getElementById('user-name');
-    const carouselItem = document.getElementById('carousel-item')
+
+
+//  renderizar apenas 1 de cada vez / uma opcao é fazer assim
+// O template já está no HTML
+function renderTestimonial(testimonial) {
+    console.log(testimonial)
+    const userNameEl = document.getElementById('testimonial-user-name');
+    const avatarEl = document.getElementById('testimonial-avatar');
+    const testimonialEl = document.getElementById('testimonial');
+    const testimonialDateEl = document.getElementById('testimonial-date');
+    userNameEl.textContent = testimonial.userName;
+    avatarEl.src = testimonial.avatar;
+    testimonialEl.textContent = testimonial.review
+    testimonialDateEl.textContent = testimonial.date
+    //`text ${variavel} continue text`
+}
+
+// 1. Renderizar todos de uma vez e manter apenas 1 visivel e os outros invisíveis 
+// ou  e substituir a cada click intervalo de tempo
+// O template tem que ser criado no JS
+
+function testimonialTemplate(testimonial) {
+    let className = '';
+    if (testimonial.id == 0) {
+        className = 'active';
+    }
+
+    return `
+    <div id="carousel-item" class="carousel-item ${className}" data-id="${testimonial.id}">
+        <div class="carousel-avatar">
+            <img id="testimonial-avatar" src="${testimonial.avatar}" alt="avatar" />
+        </div>
+        <p id="testimonial-user-name">${testimonial.userName}</p>
+        <p id="testimonial">${testimonial.review}</p>
+        <p id="testimonial-date">${testimonial.date}</p>
+    </div>`
+}
+
+function renderTestimonials(testimonialsData) {
+    const carousel = document.getElementById('carousel');
+    console.log(carousel);
+    carousel.innerHTML = ''
+    // para cada informacao eu puxo o template
     testimonialsData.forEach(testimonial => {
-        console.log(testimonial)
-        userNameEl.textContent = `${testimonial.userName}`
-        carouselItem.appendChild(userNameEl)
+        let template = testimonialTemplate(testimonial);
+        // inner pq é texto e nao um elemento (se fosse el seria append)
+        carousel.innerHTML += template;
     })
 }
 
-// function renderUserName(testimonialsData){
-//     const userNameEl = document.getElementById('user-name');
-//     userNameEl.textContent = `${testimonialsData.userName}`;
-//     carouselItem.appendChild(userNameEl)
-// }
 
-// function renderTestimonial(testimonialsData){
-//     const carouselItem = document.getElementById('carouselItem')
-//     testimonialsData.forEach(testimonial =>{
-//         carouselItem.innerHTML += testimonial
-//     })
-// }
+renderTestimonials(testimonialsData);
 
-// renderUserName()
-renderTestimonial()
-console.log(renderTestimonial(testimonialsData))
+setInterval(function () {
+    const testimonialBulletBefore = document.querySelector(`.testimonial-bullet[data-id='${currentTestimonial}']`);
+    testimonialBulletBefore.classList.remove('active');
 
-export default renderUserName
+    const testimonialItemBefore = document.querySelector(`.carousel-item[data-id='${currentTestimonial}']`);
+    testimonialItemBefore.classList.remove('active');
+
+    currentTestimonial += 1;
+    if (currentTestimonial >= testimonialsData.length) {
+        currentTestimonial = 0;
+    }
+
+    const testimonialItemAfter = document.querySelector(`.carousel-item[data-id='${currentTestimonial}']`);
+    testimonialItemAfter.classList.add('active');
+
+    const testimonialBulletAfter = document.querySelector(`.testimonial-bullet[data-id='${currentTestimonial}']`);
+    testimonialBulletAfter.classList.add('active');
+}, 3000)
+
+// aqui eu to dizendo qual o testimonnial eu estou chamando , o primerio.
+// renderTestimonial(testimonialsData[currentTestimonial]);
+
+
+// setInterval(function () {
+//     const testimonialBulletBefore = document.querySelector(`.testimonial-bullet[data-id='${currentTestimonial}']`);
+
+//     testimonialBulletBefore.classList.remove('active');
+//     currentTestimonial += 1;
+//     if (currentTestimonial >= testimonialsData.length) {
+//         currentTestimonial = 0;
+//     }
+//     renderTestimonial(testimonialsData[currentTestimonial]);
+//     const testimonialBulletAfter = document.querySelector(`.testimonial-bullet[data-id='${currentTestimonial}']`);
+//     testimonialBulletAfter.classList.add('active');
+// }, 3000)
+
