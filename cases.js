781f8d3e3b779880ec616dcdfa0733aa53e6845d
nathalia <nathaliatroina@gmail.com>
2025-08-03 21:10:28 -0400

    scrollupbtn

--- cases.js
+++ cases.js
@@ -4,13 +4,17 @@
 const scrollToTopBtn = document.getElementById("scroll-to-top-btn");
 
 // When the user scrolls down, show or hide the button
-window.onscroll = function() {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+window.addEventListener('scroll', function () {
+  
+  let top = document.documentElement.scrollTop;
+  let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+  if (top > height - 100) {
     scrollToTopBtn.style.display = "block";
   } else {
     scrollToTopBtn.style.display = "none";
   }
-};
+});
 
 // When the user clicks on the button, scroll to the top of the document
 // const scrollToTopBtn = document.getElementById("scroll-to-top-btn");
